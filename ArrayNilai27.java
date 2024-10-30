1fdbe1d0befbcc397599a5dd4f9605423850c88b
sileysa <sileysafaedatulnuraini984@gmail.com>
2024-10-30 15:02:30 +0700

    ArrayNilai27

--- ArrayNilai27.java
+++ ArrayNilai27.java
@@ -4,13 +4,18 @@ public class ArrayNilai27 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int [] nilaiAkhir = new int[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
             
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]); 
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " lulus!");
+            }else{
+                System.out.println("Mahasiswa ke-" + i + " tidak lulus!");
+            }
+            //System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]); 
         }
     }
 }
