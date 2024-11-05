94847031b7d2345ca02455d179c43dc03484cf49
sileysa <sileysafaedatulnuraini984@gmail.com>
2024-11-05 22:29:47 +0700

    SearchNilai27

--- SearchNilai27.java
+++ SearchNilai27.java
@@ -3,18 +3,31 @@ import java.util.Scanner;
 public class SearchNilai27 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
 
-        for (int i = 0; i < arrNilai.length; i++) {
+        System.out.print("Masukkan banyaknya nilai yang akan diinput : ");
+        int n = sc.nextInt();
+
+        int[] arrNilai = new int[n];
+       
+        for (int i = 0; i < n; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1)+ " : ");
+            arrNilai[i] = sc.nextInt();
+        }
+        System.out.println();
+        System.out.print("Masukkan nilai yang ingin dicari : ");
+        int key = sc.nextInt();
+
+        int hasil = -1;
+        for (int i = 0; i < n; i++) {
             if (key == arrNilai[i]) {
                 hasil = i;
                 break;
             }
         }
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.out.println();
+        if (hasil == -1) {
+            System.out.println("Nilai " + key + " tidak ditemukan.");
+        } else{
+            System.out.println("Nilai "+ key+ " ketemu, merupakan nilai mahasiswa ke-" + hasil);
+        }
     }
 }
